f0f0da3b226e78f1794868c304eb3f273f9b46fa
averagehat <bigmwp@gmail.com>
2019-07-31 17:56:09 -0400

    types work

--- ngs_doit/dodo.py
+++ ngs_doit/dodo.py
@@ -1,9 +1,6 @@
-from types import Fasta, Fastq, Bam, Bai, VCF, PNG ,FPath, Sam
-from typing import List, Tuple, Dict, Callable,Union, Any,Optional
-from typing_extensions import TypedDict
-
-
-Job = Dict[str, Any]
+from ngs_doit.pipetypes import Fasta, Fastq, Bam, Bai, VCF, PNG ,FPath, Sam, P
+from typing import List, Tuple, Dict, Callable,Union, Any,Optional, TypeVar, Type
+from typing_extensions import TypedDict, Literal
 
 # the type for get_var is something like
 # def get_var(s: str, default: T) -> Union[str, T] (the same as dict.get())
@@ -15,6 +12,12 @@ import os
 
 import yaml
 
+#Job = Dict[str, Any]
+#P = TypeVar('P', bound=FPath)
+#Action = Union[str, Callable[[List[Type[P]], List[FPath]], None]]
+Action = Union[str, Callable[[List[FPath], List[FPath]], ActionReturn]]
+ActionReturn = Union[Failure, None, Dict[str, bool]]
+
 # could use "Old" and f"" style string will work 
 # TODO: the below doesn't work@
 DOIT_CONFIG = {'action_string_formatting': 'both'}
@@ -24,13 +27,14 @@ Args = TypedDict('Args',
           'outdir' : str,
           'ref'    : Optional[str],
           'R1'     : Optional[str],
-          'R2'     : Optional[str] })
+          'R2'     : Optional[str],
+          'sample' : Optional[str]})
 args: Args = {"config": get_var('config', 'config.yaml'),
           "outdir": get_var('outdir', './re-out'),
           "ref"   : get_var('ref', None),
           "R1"    : get_var('r1', None),
-          'R2'    : get_var('r2', None)}
-
+          'R2'    : get_var('r2', None),
+          'sample': get_var('sample', None)}
 
 # these file names could be dynamically named after the sample by adding a "sample" value to
 # the `Args` dict and using that within each relevant task's closure! 
@@ -43,23 +47,45 @@ unsorted_bam = Bam('bwa.bam')
 sorted_bam = Bam("sorted.bam")
 index_bai = sorted_bam.bai
 # could name below in a standard way but this is better
+IntValue = TypedDict('IntValue', { 'default' : int })
+StringValue = TypedDict('StringValue', { 'default' : str })
+BoolValue = TypedDict('BoolValue', { 'default' : bool })
+Trim = TypedDict('Trim', {'q' : IntValue , 'headcrop' : IntValue })
+Fqstats = TypedDict('Fqstats', {'skip' : BoolValue })
+Config = TypedDict('Config', 
+        { 'trim_reads' :  Trim,
+    'fqstats' : Fqstats
+    })
+from typing import cast
 
-def task_trimmomatic() -> Union[Dict[str, Any], bool]:
+Job = TypedDict('Job', 
+        { 'file_dep' : List[FPath],
+          'targets' : List[FPath],
+          'actions' : List[Action] })
+
+GetArgs = Dict[str, Tuple[str, str]]
+GetArgsJob = TypedDict('GetArgsJob', { 'getargs' : GetArgs, 
+         'file_dep' : List[FPath],
+          'targets' : List[FPath],
+          'actions' : List[Action] })
+Failure = Literal[False]
+def task_trimmomatic() -> Union[Job, Failure]:
     # could also have this at the "main level" of the file :(
-    cfg = yaml.load(open(args['config']))
+    cfg: Config = yaml.load(open(args['config']))
     q  = cfg['trim_reads']['q']['default']
     hc = cfg['trim_reads']['headcrop']['default']
-    r1 = args['R1']
-    r2 = args['R2'] 
-    if r1 is None or r2 is None:
+    r1_ = args['R1']
+    r2_ = args['R2'] 
+    if r1_ is None or r2_ is None:
         return False # some other way of indicating failure
+    r1, r2 = Fastq(r1_), Fastq(r2_)
+    def run_trim(targets: List[FPath], dependencies: List[FPath]) -> Dict[str, bool]:
+        nonempty: Callable[[FPath],bool] = lambda f: os.stat(f).st_size > 0 
+        subprocess.getoutput(f"trimmomatic PE {dependencies[0]} {dependencies[1]} \
+                %(targets)s LEADING:{q} TRAILING:{q} HEADCROP:{hc}")
+        return { 'unpaired-reads' : nonempty(targets[1]) or nonempty(targets[3]) }
 
-    def run_trim(targets: List[str], dependencies: List[str]) -> None:
-        subprocess.getoutput(f"trimmomatic PE {' '.join(dependencies)} \
-                {targets[0]} unpaired_fw.fq {targets[1]} unpaired_rev.fq \
-                LEADING:{q} TRAILING:{q} HEADCROP:{hc}")
-
-    return { 'targets' : [ FPath('trimmed').fastq, FPath('Trimmed.R2').fastq],
+    return { 'targets' : [ FPath('trimmed.R1').fastq,  Fastq('unpaired_trimmed_R1.fastq'), FPath('Trimmed.R2').fastq, Fastq('unpaired_trimmed_R2.fastq')],
              'file_dep' : [r2, r1],
              'actions' : [run_trim] }
 
@@ -76,8 +102,8 @@ def task_bwa_mem() -> Job:
 
     # these are actually paths right?
     # let sorting be a seperator process 
-    def bwa_mem(targets: List[str], dependencies: List[str]) -> None:
-        nonempty: Callable[[str],bool] = lambda f: os.stat(f).st_size > 0 
+    def bwa_mem(targets: List[FPath], dependencies: List[FPath]) -> None:
+        nonempty: Callable[[FPath],bool] = lambda f: os.stat(f).st_size > 0 
         r1, r2, u1, u2 = targets
         paired_bam =   Bam('paired.bam')
         unpaired_bam = Bam('unpaired.bam')
@@ -95,30 +121,54 @@ def task_bwa_mem() -> Job:
         else:
             map_paired(final_bam) # but doesn't sort :(
 
-    return { 'targets' : unsorted_bam,
+    return { 'targets' : [unsorted_bam],
              'file_dep' : index_files + [trimmed_r1, trimmed_r2, up_trimmed1, up_trimmed2], 
              'actions' : [ bwa_mem ] }
 
 def task_sort_bam() -> Job:
     # how can these targets and dependencies be parameterizable like they are in, say, `dagr`?
-    return { 'targets' : sorted_bam,
-            'file_dep' :  unsorted_bam,
+    return { 'targets' : [sorted_bam],
+            'file_dep' : [unsorted_bam],
             'actions'  : [ "samtools sort %(dependencies)s -f %(targets)s" ] }
 
 def task_index_bam() -> Job: 
 
-    return { 'targets'  : index_bai,
-             'file_dep' : sorted_bam,
+    return { 'targets'  : [index_bai],
+             'file_dep' : [sorted_bam],
              'actions'  : [ "samtools index %(dependencies)s" ] }
 
-
 def task_fqstats() -> Job:
-    cfg = yaml.load(open(args['config']))
-    skip_stats = cfg['fqstats']['skip']['default'] # or accept commandline arg
-
+    cfg: Config = yaml.load(open(args['config']))
+    sample = 'SAMPLE' # args.get('sample', 'SAMPLE')
+    skip_stats = cfg['fqstats']['skip']['default'] # or accept commandline arg 
     if skip_stats:
         return { 'targets' : [], 'file_dep' : [], 'actions': []}
     else: 
-        return { 'targets' : ["foo.fqstat"], 'file_dep' : [], 'actions' : ['fqstats bleh bleh'] }
+        return { 'targets' : [FPath(sample+ '_fqstat').png],
+                'file_dep' : [trimmed_r1, trimmed_r2], 
+                'actions' : ['fqstats -o %(targets)s %(dependencies)s'] }
+# cmd1 = 'lofreq call-parallel --pp-threads ' + str(threads) + ' -f {reference} {bamfile} -o {vcf} ' + lofreq_options
+
+def task_lofreq_index_ref() -> Job:
+
+    return { 'targets' : [ref.fai],
+            'actions' : ["lofreq faidx %(dependencies)s"],
+            'file_dep' : [ ref ] }
+def task_call_variants() -> Job:
+ caller = 'lofreq'
+ threads = 2
+ lofreq_vcf = VCF('lofreq.vcf')
+
+ if caller is 'lofreq':
+  return { 'targets' : [lofreq_vcf],
+           'file_dep' : [sorted_bam, ref, ref.fai, index_bai],
+           'actions' : [ # TODO: must bea  better way to give it individual dependency files
+           f"lofreq call-parallel --pp-threads {threads} -f {ref} {sorted_bam} -o %(targets)s" # + lofreq-options
+           ] }
+
+ else:
+     return cast(Job, {})
+
+
 
 
--- ngs_doit/pipetypes.py
+++ ngs_doit/pipetypes.py
@@ -1,14 +1,15 @@
-from typing_extensions import Protocol, TypedDict
+from typing_extensions import Protocol, TypedDict, final, Final
 from typing import TypeVar, Generic, List, Tuple, Union
 # doesn't really work becuase the semantics (what we really mean)
 # is that targets is a list which includes at least one Bam file. 
 import pathlib
 
 
-T = TypeVar('T', bound='FPath')
+P = TypeVar('P', bound='FPath')
 class FPath(pathlib.PurePosixPath):
 
-    def add_suffix(self: 'T', suf: str) -> 'T':
+    @final
+    def add_suffix(self: 'P', suf: str) -> 'P':
         return self.with_suffix(self.suffix + suf)
 
     @property
@@ -32,6 +33,10 @@ class FPath(pathlib.PurePosixPath):
         return Sam(self.add_suffix('.sam'))
 
     @property
+    def fai(self) -> 'Fai': 
+        return Fai(self.add_suffix('.fai'))
+
+    @property
     def vcf(self) -> 'VCF': 
         return VCF(self.add_suffix('.vcf'))
 
@@ -40,7 +45,7 @@ class FPath(pathlib.PurePosixPath):
         return PNG(self.add_suffix('.png'))
         # will replace old suffix
         # requires that you start suffix with '.'
-        return Fastq(self.with_suffix('.fastq'))
+        # return Fastq(self.with_suffix('.fastq'))
 
 class Fastq(FPath): ...
 class Bam(FPath): ...
@@ -49,6 +54,7 @@ class Bai(FPath): ...
 class VCF(FPath): ...
 class Fasta(FPath): ...
 class PNG(FPath): ...
+class Fai(FPath): ...
 
 PEJob = TypedDict('PEJob', { 'dependencies' : List[Tuple[Fastq, Fastq]] })
 PEMap = TypedDict('PEMap', { 'targets' : List[Bam], 'dependencies' : List[Tuple[Fastq, Fastq]] }) 
